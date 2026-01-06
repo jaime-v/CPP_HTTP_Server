0f3a4f4d19d59cfa555ffa6fc5e34bd07220a819
jaime-v <pocholov.2403@gmail.com>
2026-01-06 00:37:00 -0800

    Properly parse request lines, check for valid requests

--- src/client.cpp
+++ src/client.cpp
@@ -12,14 +12,30 @@
 #define LISTEN_BACKLOG 100
 #define BUF_SIZE 256
 
+void print_vector(std::vector<uint8_t> vector) {
+  std::cout << "\nVector print begin\n";
+  for (std::size_t i{}; i < vector.size(); ++i) {
+    printf("(%02x) ", vector[i]);
+  }
+  for (std::size_t i{}; i < vector.size(); ++i) {
+    printf("(%c) ", vector[i]);
+  }
+  std::cout << "\nVector print end\n";
+}
+
 int main(void) {
+  // Typical Socket API stuff
   struct sockaddr_in addr;
   socklen_t addr_len{sizeof(addr)};
   int socket_fd;
-  uint8_t *write_buffer = (uint8_t *)malloc(BUF_SIZE);
-  uint8_t *read_buffer = (uint8_t *)malloc(BUF_SIZE);
-  ssize_t bytes_read;
+  // ssize_t bytes_read;
   ssize_t bytes_recv;
+
+  // Create buffers for reading and writing
+  // These are vectors of uint8_t, we will use these for networking
+  // std::vector<uint8_t> read_buffer(BUF_SIZE);
+  // std::vector<uint8_t> write_buffer(BUF_SIZE);
+
   // Create socket
   socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_fd == -1) {
@@ -39,36 +55,81 @@ int main(void) {
     std::cerr << "connect error\n";
   }
 
-  // Read from user input
-  bytes_read = read(STDIN_FILENO, write_buffer, BUF_SIZE);
-  if (bytes_read == 0) {
-    std::cout << "Read in EOF\n";
-  }
-  if (bytes_read == -1) {
-    std::cout << "Read from stdin error\n";
-  }
-  // String Safety
-  write_buffer[bytes_read] = '\0';
-  std::cout << "Got message\n";
-  std::cout << write_buffer << '\n';
+  /*
+  // Read from user input into the write buffer's data
+  // C++ Style involving string for input, then converting to uint8_t
+  // push_back the null terminator for string safety
+  std::string input;
+  std::getline(std::cin, input);
+  std::vector<uint8_t> write_buffer(input.begin(), input.end());
+  write_buffer.push_back('\0');
+  print_vector(write_buffer);
+  std::cout << "Got user input\n";
 
-  // Sanitize input
+  // C Style that we used before
+  // bytes_read = read(STDIN_FILENO, write_buffer.data(), BUF_SIZE);
+  // if (bytes_read == 0) {
+  //   std::cout << "Read in EOF\n";
+  // }
+  // if (bytes_read == -1) {
+  //   std::cout << "Read from stdin error\n";
+  // }
 
   // Send user input through socket
-  if (send(socket_fd, write_buffer, bytes_read, 0) == -1) {
+  if (send(socket_fd, write_buffer.data(), write_buffer.size(), 0) == -1) {
     std::cerr << "send error\n";
   }
-  std::cout << "Sent to server\n";
+  std::cout << "Sent user input to server\n";
+  */
+
+  // Sending valid request
+  std::string message_string =
+      "GET /index.html HTTP/1.0\r\nHost: localhost\r\n\r\n";
+  std::vector<uint8_t> message_vector(message_string.begin(),
+                                      message_string.end());
+  if (send(socket_fd, message_vector.data(), message_vector.size(), 0) == -1) {
+    std::cerr << "send error\n";
+  }
+  print_vector(message_vector);
+
+  // Sending valid request with altered path
+  // std::string message_string = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+  // std::vector<uint8_t> message_vector(message_string.begin(),
+  //                                     message_string.end());
+  // if (send(socket_fd, message_vector.data(), message_vector.size(), 0) == -1)
+  // {
+  //   std::cerr << "send error\n";
+  // }
+  // print_vector(message_vector);
+
+  // Sending invalid method
+  // std::string message_string =
+  //     "DELETE /index.html HTTP/1.1\r\nHost: localhost\r\n\r\n";
+  // std::vector<uint8_t> message_vector(message_string.begin(),
+  //                                     message_string.end());
+  // if (send(socket_fd, message_vector.data(), message_vector.size(), 0) == -1)
+  // {
+  //   std::cerr << "send error\n";
+  // }
+  // print_vector(message_vector);
+
+  // Sending invalid version
+  // std::string message_string =
+  //     "GET /index.html HTTP/6.7\r\nHost: localhost\r\n\r\n";
+  // std::vector<uint8_t> message_vector(message_string.begin(),
+  //                                     message_string.end());
+  // if (send(socket_fd, message_vector.data(), message_vector.size(), 0) == -1)
+  // {
+  //   std::cerr << "send error\n";
+  // }
+  // print_vector(message_vector);
 
   // Read server response
-  if ((bytes_recv = recv(socket_fd, read_buffer, BUF_SIZE, 0)) == -1) {
+  std::vector<uint8_t> read_buffer(BUF_SIZE);
+  if ((bytes_recv =
+           recv(socket_fd, read_buffer.data(), read_buffer.size(), 0)) == -1) {
     std::cerr << "recv error\n";
   }
-  read_buffer[bytes_recv] = '\0';
   std::cout << "Received from server\n";
-  std::cout << read_buffer << '\n';
-
-  free(write_buffer);
-  free(read_buffer);
   return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,6 @@
 #include <arpa/inet.h>
 #include <iostream>
 #include <netinet/in.h>
-#include <sstream>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +14,45 @@
 #define PORT 8000
 #define LISTEN_BACKLOG 100
 #define BUF_SIZE 256
+
+void print_byte_vector(std::vector<uint8_t> vector) {
+  std::cout << "\nVector print begin\n";
+  for (std::size_t i{}; i < vector.size(); ++i) {
+    printf("(%02x) ", vector[i]);
+  }
+  for (std::size_t i{}; i < vector.size(); ++i) {
+    printf("(%c) ", vector[i]);
+  }
+  std::cout << "\nVector print end\n";
+}
+
+void print_string_vector(std::vector<std::string> vector) {
+  std::cout << "\nVector print begin\n";
+  for (std::size_t i{}; i < vector.size(); ++i) {
+    std::cout << i << "\n";
+    std::cout << vector[i] << "\n";
+    for (std::size_t j{}; j < vector[i].length(); ++j) {
+      printf("(%02x) ", vector[i][j]);
+    }
+    std::cout << "\n\n";
+  }
+  std::cout << "\nVector print end\n";
+}
+
+// I assume this modifies string internally? Doesn't look like it actually
+// since we are passing it by value, not by reference
+std::vector<std::string> tokenize_input(std::string string, std::string delim) {
+  std::vector<std::string> tokens;
+  std::string token{};
+  size_t pos = 0;
+  while ((pos = string.find(delim)) != std::string::npos) {
+    token = string.substr(0, pos);
+    tokens.push_back(token);
+    string.erase(0, pos + delim.length());
+  }
+  tokens.push_back(string);
+  return tokens;
+}
 
 int main(void) {
   struct sockaddr_in addr {};
@@ -56,7 +94,7 @@ int main(void) {
     std::cout << "Connected a client! " << client_fd << '\n';
 
     // Receive request, store in buffer
-    std::vector<uint8_t> buffer(4096);
+    std::vector<uint8_t> buffer(BUF_SIZE);
     if ((bytes_recv = recv(client_fd, buffer.data(), buffer.size(), 0)) == -1) {
       std::cerr << "recv error\n";
     }
@@ -88,34 +126,68 @@ int main(void) {
     // will represent our tokens.
     // And we have the std::string::erase method for removing tokens.
     // This is essentially a strtok_r function, but in C++
+
+    // Now, we need to tokenize based on \r\n, then we need to tokenize again
+    // based on spaces
     std::cout << "We are starting to tokenize the input\n";
-    std::vector<std::string> tokens;
-    std::string token{};
-    std::string delimiter{" "};
-    size_t pos = 0;
-    while ((pos = request.find(delimiter)) != std::string::npos) {
-      token = request.substr(0, pos);
-      tokens.push_back(token);
-      request.erase(0, pos + delimiter.length());
+    std::vector<std::string> lines = tokenize_input(request, "\r\n");
+    std::cout << "We tokenized the input based on lines\n";
+    print_string_vector(lines);
+
+    // Create a 2-D vector
+    // Vector contains vectors of strings
+    // Vector 0 is line 0 (GET /index.html HTTP/1.1), Vector 1 is line 1, etc.
+    // Expect vector 0 to have 3 tokens, vector 1 to have 2 tokens
+    std::cout << "Starting to tokenize first line based on spaces\n";
+    std::vector<std::string> request_line = tokenize_input(lines[0], " ");
+    std::cout << "We tokenized the input based on spaces\n";
+
+    std::cout << "Displaying tokens of request line\n";
+    print_string_vector(request_line);
+    std::cout << "End of tokens of request line\n";
+
+    std::string method = request_line[0];
+    std::string path = request_line[1];
+    std::string version = request_line[2];
+
+    if (method != "GET") {
+      std::cout << "Respond with 405 - Method Not Allowed\n";
     }
-    tokens.push_back(request);
-    std::cout << "We tokenized the input\n";
+    if (version != "HTTP/1.1" && version != "HTTP/1.0") {
+      std::cout << "Respond with 505 - HTTP Version Not Supported\n";
+    }
+    if (path == "/") {
+      std::cout << "Path is just /, swapping to /index.html\n";
+      path = "/index.html";
+    }
+
+    // Adjust path based on file structure
+    std::string doc_root = "./files";
+    path.insert(0, doc_root);
+    std::cout << "Updated path:";
+    std::cout << path << '\n';
+
+    // If file exists, open it and send response
+    // If file does not exist, send 404
+
+    // Tokenizing based on spaces
+    // std::vector<std::string> tokens = tokenize_input(request, " ");
+    // print_string_vector(tokens);
+
     // Tokens should now contain all tokens
-    // We create tokens vector, create a null token, and a delimiter we want
-    // Start at pos 0
-    // We find delimiter and as long as its not equal to npos (no position?)
-    // We create a substring from pos 0 to the delimiter
-    // We then push that substring onto the vector
-    // Then we erase the token and delimiter from buffer
-    // Then we repeat the cycle until our pos hits npos
-    // Once we find npos, that means the delimiter was not found
-    // Then buffer only has the last token
-    // So we add it to tokens and yeah
-    std::cout << "Displaying tokens\n";
-    for (size_t i{}; i < tokens.size(); ++i) {
-      std::cout << tokens[i] << '\n';
-    }
-    std::cout << "End of tokens\n";
+    // We create tokens vector, create a null token, and a delimiter we
+    // want Start at pos 0 We find delimiter and as long as its not
+    // equal to npos (no position?) We create a substring from pos 0 to
+    // the delimiter We then push that substring onto the vector Then we
+    // erase the token and delimiter from buffer Then we repeat the
+    // cycle until our pos hits npos Once we find npos, that means the
+    // delimiter was not found Then buffer only has the last token So we
+    // add it to tokens and yeah
+    // std::cout << "Displaying tokens\n";
+    // for (size_t i{}; i < tokens.size(); ++i) {
+    //   std::cout << tokens[i] << "\n";
+    // }
+    // std::cout << "End of tokens\n";
 
     // Process request
     // Send response
